884dbfd64047b4a9ca0b00154574ce91bb7507e2
navigt2000 <navigt2000@gmail.com>
2024-12-03 14:18:10 +0100

    PhoneMode script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const slides = document.querySelector('.slides');
-const images = document.querySelectorAll('.slides img');
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
+const slides = document.querySelector('.image-s2 .carousel .slides');
+const images = document.querySelectorAll('.image-s2 .carousel .slides img');
+const prevButton = document.querySelector('.image-s2 .carousel .prev');
+const nextButton = document.querySelector('.image-s2 .carousel .next');
+
+const slides2 = document.querySelector('.image-s2_2 .carousel .slides');
+const images2 = document.querySelectorAll('.image-s2_2 .carousel .slides img');
+const prevButton2 = document.querySelector('.image-s2_2 .carousel .prev');
+const nextButton2 = document.querySelector('.image-s2_2 .carousel .next');
 
 let currentIndex = 0;
-
-console.log("next")
 
 function showSlide(index) {
     const slideWidth = images[0].clientWidth;
@@ -22,8 +25,25 @@ function prevSlide() {
     showSlide(currentIndex);
 }
 
+function showSlide2(index) {
+    const slideWidth = images2[0].clientWidth;
+    slides2.style.transform = `translateX(-${index * slideWidth}px)`;
+}
+
+function nextSlide2() {
+    currentIndex = (currentIndex + 1) % images2.length;
+    showSlide2(currentIndex);
+}
+
+function prevSlide2() {
+    currentIndex = (currentIndex - 1 + images2.length) % images2.length;
+    showSlide2(currentIndex);
+}
+
 nextButton.addEventListener('click', nextSlide);
 prevButton.addEventListener('click', prevSlide);
+nextButton2.addEventListener('click', nextSlide2);
+prevButton2.addEventListener('click', prevSlide2);
 
 // Cambia de imagen automáticamente cada 5 segundos
 setInterval(nextSlide, 10000);
